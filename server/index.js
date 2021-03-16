eb485299c2d81423496e0c51dfbccf16845f0d67
Karim Zamrini <karim.e.zamrini@gmail.com>
2021-03-15 18:01:27 -0600

    server reminders

---
+++ server/index.js
@@ -0,0 +1,8 @@
+// Express node server
+
+// will listen for GET requests on four endpoints:
+// GET /products Retrieves the list of products.
+// GET /products/:product_id Returns all product level information for a specified product id.
+// GET /products/:product_id/styles Returns the all styles available for the given product.
+// GET /products/:product_id/related Returns the id's of products related to the product specified.
+// refer to https://learn-2.galvanize.com/cohorts/2480/blocks/94/content_files/Front%20End%20Capstone/project-atelier-catwalk/products.md for more information
