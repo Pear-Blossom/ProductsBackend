00452054a0b36f9da15ec17ac81e06124fc38b27
Karim Zamrini <karim.e.zamrini@gmail.com>
2021-03-19 16:44:09 -0600

    related product query

--- database_postgres/index.js
+++ database_postgres/index.js
@@ -9,6 +9,7 @@ const pool = new Pool({
 });
 
 // // GET /products
+// // TODO CHANGE ID TO REQ PARAM
 // const getProducts = (req, res) => {
 //   const page = 1;
 //   const count = 5;
@@ -25,7 +26,7 @@ const pool = new Pool({
 // }
 
 // // GET /products/:product_id
-// // TODO: JOIN TABLE WITH FEATURES WITH COLUMN FEATURE AND VALUE WHERE ID IS THE SAME
+// // TODO: JOIN TABLE WITH FEATURES WITH COLUMN FEATURE AND VALUE WHERE ID IS THE SAME + CHANGE ID TO REQ PARAM
 // const getProductById = (req, res) => {
 //   const id = parseInt(req.params.product_id)
 //   const id = 15
@@ -34,32 +35,45 @@ const pool = new Pool({
 //     .catch((err) => { res.send(err) })
 // }
 
-// GET /products/:product_id/styles
-// TODO JOIN WITH PHOTOS AND SKUS FOR A GIVEN PRODUCT AND NEST THEM IN EACH STYLE OBJECT
-const getStyles = (req, res) => {
-  const product_id = 15
-  const stylesObj = {
-    product_id: product_id,
-    results: []
-  }
-  pool.query(`SELECT * FROM styles WHERE product_id = ${product_id}`)
-    // .then((results) => { console.log('product id result: ', results.rows); res.send(results.rows) })
-    .then((results) => {
-      results.rows.forEach((row) => {
-        let styleObj = {
-          style_id: row.id,
-          name: row.name,
-          original_price: row.original_price,
-          sale_price: row.sale_price === 'null' ? '0' : row.sale_price,
-          'default?': row.default_style === '1' ? true : false,
-          photos: [],
-          skus: {}
-        }
-        stylesObj.results.push(styleObj);
-      })
-      console.log('styles object: ', stylesObj)
-    })
-    .catch((err) => { console.log('error:', err); res.send(err) })
-}
-getStyles();
+// // GET /products/:product_id/styles
+// // TODO JOIN WITH PHOTOS AND SKUS FOR A GIVEN PRODUCT AND NEST THEM IN EACH STYLE OBJECT + CHANGE ID TO REQ PARAM
+// const getStyles = (req, res) => {
+//   const product_id = 15
+//   const stylesObj = {
+//     product_id: product_id,
+//     results: []
+//   }
+//   pool.query(`SELECT * FROM styles WHERE product_id = ${product_id}`)
+//     // .then((results) => { console.log('product id result: ', results.rows); res.send(results.rows) })
+//     .then((results) => {
+//       results.rows.forEach((row) => {
+//         let styleObj = {
+//           style_id: row.id,
+//           name: row.name,
+//           original_price: row.original_price,
+//           sale_price: row.sale_price === 'null' ? '0' : row.sale_price,
+//           'default?': row.default_style === '1' ? true : false,
+//           photos: [],
+//           skus: {}
+//         }
+//         stylesObj.results.push(styleObj);
+//       })
+//       console.log('styles object: ', stylesObj)
+//     })
+//     .catch((err) => { console.log('error:', err); res.send(err) })
+// }
+
 // GET /products/:product_id/related Returns the id's of products related to the product specified.
+const getRelated = (req, res) => {
+  const product_id = 15;
+  const relatedArray = [];
+  pool.query(`SELECT related_product_id FROM related WHERE current_product_id = ${product_id}`)
+  .then((results) => {
+    results.rows.forEach((row) => {
+      relatedArray.push(row.related_product_id)
+    })
+    console.log('related products: ', relatedArray)
+  })
+  .catch((err) => { console.log('error:', err); res.send(err) })
+}
+getRelated();
