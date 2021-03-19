6eed9a4e4ce2d66e0c8eacd05d4d104a57ea415f
Karim Zamrini <karim.e.zamrini@gmail.com>
2021-03-19 16:37:41 -0600

    styles query

--- database_postgres/index.js
+++ database_postgres/index.js
@@ -8,28 +8,58 @@ const pool = new Pool({
   database: "products"
 });
 
-// GET /products Retrieves the list of products.
-const getProducts = (req, res) => {
-  const page = 1;
-  const count = 5;
-  if (req) {
-    page = parseInt(req.params.page);
-  }
-  if (req) {
-    count = parseInt(req.params.count);
-  }
-  const limit = page * count
-  pool.query(`SELECT * FROM products LIMIT ${limit}`)
-    .then((results) => { console.log('product results: ', results.rows); res.send(results.rows) })
-    .catch((err) => { res.send(err) })
-}
+// // GET /products
+// const getProducts = (req, res) => {
+//   const page = 1;
+//   const count = 5;
+//   if (req) {
+//     page = parseInt(req.params.page);
+//   }
+//   if (req) {
+//     count = parseInt(req.params.count);
+//   }
+//   const limit = page * count
+//   pool.query(`SELECT * FROM products LIMIT ${limit}`)
+//     .then((results) => { console.log('product results: ', results.rows); res.send(results.rows) })
+//     .catch((err) => { res.send(err) })
+// }
 
-// GET /products/:product_id Returns all product level information for a specified product id.
-const getProductById = (req, res) => {
-  const id = parseInt(req.params.product_id)
-  client.query(`SELECT * FROM products WHERE id = ${id}`)
-    .then((results) => { console.log('product id result: ', results.rows); res.send(results.rows) })
-    .catch((err) => { res.send(err) })
+// // GET /products/:product_id
+// // TODO: JOIN TABLE WITH FEATURES WITH COLUMN FEATURE AND VALUE WHERE ID IS THE SAME
+// const getProductById = (req, res) => {
+//   const id = parseInt(req.params.product_id)
+//   const id = 15
+//   pool.query(`SELECT * FROM products WHERE id = ${id}`)
+//     .then((results) => { console.log('product id result: ', results.rows); res.send(results.rows) })
+//     .catch((err) => { res.send(err) })
+// }
+
+// GET /products/:product_id/styles
+// TODO JOIN WITH PHOTOS AND SKUS FOR A GIVEN PRODUCT AND NEST THEM IN EACH STYLE OBJECT
+const getStyles = (req, res) => {
+  const product_id = 15
+  const stylesObj = {
+    product_id: product_id,
+    results: []
+  }
+  pool.query(`SELECT * FROM styles WHERE product_id = ${product_id}`)
+    // .then((results) => { console.log('product id result: ', results.rows); res.send(results.rows) })
+    .then((results) => {
+      results.rows.forEach((row) => {
+        let styleObj = {
+          style_id: row.id,
+          name: row.name,
+          original_price: row.original_price,
+          sale_price: row.sale_price === 'null' ? '0' : row.sale_price,
+          'default?': row.default_style === '1' ? true : false,
+          photos: [],
+          skus: {}
+        }
+        stylesObj.results.push(styleObj);
+      })
+      console.log('styles object: ', stylesObj)
+    })
+    .catch((err) => { console.log('error:', err); res.send(err) })
 }
-// GET /products/:product_id/styles Returns the all styles available for the given product.
+getStyles();
 // GET /products/:product_id/related Returns the id's of products related to the product specified.
